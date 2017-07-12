87ebbaa349b7d9428f8f0fb31132b0d825e43e78
Aarushi Goyal <agoyal@grepruby.com>
2017-07-12 15:59:34 +0530

    Assignment4 changes done

--- Assignment4/Assignment4.js
+++ Assignment4/Assignment4.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 
-function checkError(error){
-  if(error)
+function checkError(error) {
+  if(error) {
+    print(error);
     throw error;
+  }
 }
 
-function print(string)
-{
+function print(string) {
   console.log(string);
 }
 
 function readAppendWrite(readFile, appendFile, writeFile) {
-  fs.readFile (readFile, function (errorOnFile1Read, dataFromFile1) {
-    checkError (errorOnFile1Read); 
-    print(dataFromFile1.toString())
+  fs.readFile(readFile, function (errorOnFile1Read, dataFromFile1) {
+    checkError(errorOnFile1Read); 
+    print(dataFromFile1.toString());
     print("----------------------------------------------");
 
     fs.appendFile(appendFile, dataFromFile1, function (errorOnFile2Append) {
@@ -23,7 +24,7 @@ function readAppendWrite(readFile, appendFile, writeFile) {
         checkError(errorOnFile2Read);
         
         fs.writeFile(writeFile, datafromFile2, function (errorOnFile3Write) {
-          checkError(errorOnFile3Write)
+          checkError(errorOnFile3Write);
           print(data2.toString());
           print("-----------------------------------------------");
         });
