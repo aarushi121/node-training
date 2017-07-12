817c68aa0c227b3e5e44f37cb269557c40aaa395
Aarushi Goyal <agoyal@grepruby.com>
2017-07-12 15:29:19 +0530

    Changes done on Callback Assignment

--- Assignment4/Assignment4.js
+++ Assignment4/Assignment4.js
@@ -1,21 +1,31 @@
 const fs = require('fs');
 
-function readAppendWrite (readFile, appendFile, writeFile) {
-  fs.readFile (readFile, function (err1, data1) {
-    if (err1) throw err1;
-    console.log(data1.toString())
-    console.log("----------------------------------------------");
+function checkError(error){
+  if(error)
+    throw error;
+}
 
-    fs.appendFile(appendFile, data1, function (err2) {
-      if (err2) throw err2;
+function print(string)
+{
+  console.log(string);
+}
+
+function readAppendWrite(readFile, appendFile, writeFile) {
+  fs.readFile (readFile, function (errorOnFile1Read, dataFromFile1) {
+    checkError (errorOnFile1Read); 
+    print(dataFromFile1.toString())
+    print("----------------------------------------------");
+
+    fs.appendFile(appendFile, dataFromFile1, function (errorOnFile2Append) {
+      checkError(errorOnFile2Append);
   
-      fs.readFile (appendFile, function (err3, data2) {
-        if (err3) throw err3;
+      fs.readFile(appendFile, function (errorOnFile2Read, dataFromFile2) {
+        checkError(errorOnFile2Read);
         
-        fs.writeFile(writeFile, data2, function (err3) {
-          if (err2) throw err2;
-          console.log(data2.toString());
-          console.log("-----------------------------------------------");
+        fs.writeFile(writeFile, datafromFile2, function (errorOnFile3Write) {
+          checkError(errorOnFile3Write)
+          print(data2.toString());
+          print("-----------------------------------------------");
         });
       });
     });
