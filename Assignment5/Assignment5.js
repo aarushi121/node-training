d2c7417ade8a4b9bae31c9c3df933ed86e7c03da
Aarushi Goyal <agoyal@grepruby.com>
2017-07-18 13:04:46 +0530

    Assignment5 suggested changes done

--- Assignment5/Assignment5.js
+++ Assignment5/Assignment5.js
@@ -1,38 +1,27 @@
 const fs = require('fs');
-var read = function(readFile) {
-  return new Promise((resolve,reject) => {
-    fs.readFile(readFile, function(error, dataFromFile) {
-      if (error)
-        reject(error);
-      resolve(dataFromFile);
-      console.log("------------------------------------------------------------");
-      console.log(dataFromFile.toString());
-    });
-  });
-};
-var append = function(appendFile, dataFromFile) {
-  return new Promise((resolve,reject) => {
-    fs.appendFile(appendFile, dataFromFile.toString(), function(error) {
-      if (error)
-        reject(error);
-      resolve();
-    });
-  });
-};
-var write = function(writeFile, dataFromFile) {
-  return new Promise((resolve,reject) => {
-    fs.writeFile(writeFile, dataFromFile, function(error) {
-      if (error)
-        reject(error);
-      resolve();
-    });
-  });
-};
 
-read('file1.txt')
- .then(result => { return append('file2.txt', result) })
- .then(result => { return read('file2.txt') })
- .then(result => { return write('file3.txt', result) })
- .then(result => { return read('file3.txt') })
- .catch(error => console.log(error))
-
+var makePromise = (fileName, fileOperation, tempData) => {
+  return new Promise((resolve, reject) => {
+    fs[fileOperation](fileName, tempData, (error, dataFromFile) => {
+      error ? reject(error) : resolve(dataFromFile);
+    })
+  })
+}
+var print = (string) => {
+  console.log(string);
+}
+makePromise('file1.txt', "readFile", "")
+ .then(result => { print("---------------------------------------------");
+                   print(result.toString());
+                   return makePromise('file2.txt', "appendFile", result) 
+                 })
+ .then(result => { return makePromise('file2.txt', "readFile", "") })
+ .then(result => { print("----------------------------------------------");
+                   print(result.toString());
+                   return makePromise('file3.txt', "writeFile", result)
+                 })
+ .then(result => { return makePromise('file3.txt', "readFile", "") })
+ .then(result => { print("----------------------------------------------");
+                   print(result.toString());
+                 })
+ .catch(error => print(error));
