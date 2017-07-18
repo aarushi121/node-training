d30578576358f0b418f4ebd00c77c678060c4f00
Aarushi Goyal <agoyal@grepruby.com>
2017-07-18 15:49:21 +0530

    Assignment5 fix-indentation,return statement,variable dec

--- Assignment5/Assignment5.js
+++ Assignment5/Assignment5.js
@@ -1,27 +1,33 @@
 const fs = require('fs');
 
-var makePromise = (fileName, fileOperation, tempData) => {
+const makePromise = (fileName, fileOperation, tempData) => {
   return new Promise((resolve, reject) => {
     fs[fileOperation](fileName, tempData, (error, dataFromFile) => {
       error ? reject(error) : resolve(dataFromFile);
     })
   })
 }
-var print = (string) => {
+
+const print = (string) => {
   console.log(string);
 }
+
 makePromise('file1.txt', "readFile", "")
- .then(result => { print("---------------------------------------------");
-                   print(result.toString());
-                   return makePromise('file2.txt', "appendFile", result) 
-                 })
- .then(result => { return makePromise('file2.txt', "readFile", "") })
- .then(result => { print("----------------------------------------------");
-                   print(result.toString());
-                   return makePromise('file3.txt', "writeFile", result)
-                 })
- .then(result => { return makePromise('file3.txt', "readFile", "") })
- .then(result => { print("----------------------------------------------");
-                   print(result.toString());
-                 })
+ .then(result => { 
+    print("---------------------------------------------");
+    print(result.toString());
+    return makePromise('file2.txt', "appendFile", result) 
+  })
+ .then(result => makePromise('file2.txt', "readFile", "") )
+ .then(result => { 
+    print("----------------------------------------------");
+    print(result.toString());
+    return makePromise('file3.txt', "writeFile", result)
+  })
+ .then(result => makePromise('file3.txt', "readFile", "") )
+ .then(result => { 
+    print("----------------------------------------------");
+    print(result.toString());
+  })
  .catch(error => print(error));
+
