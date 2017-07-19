d4a95dba17e58b80e72be90af036973ba56cced3
Aarushi Goyal <agoyal@grepruby.com>
2017-07-19 20:08:17 +0530

    Minor changes done-Assignment5

--- Assignment5/Assignment5.js
+++ Assignment5/Assignment5.js
@@ -4,27 +4,25 @@ const makePromise = (fileName, fileOperation, tempData) => {
   return new Promise((resolve, reject) => {
     fs[fileOperation](fileName, tempData, (error, dataFromFile) => {
       error ? reject(error) : resolve(dataFromFile);
-    })
-  })
+    });
+  });
 }
 
-const print = (string) => {
-  console.log(string);
-}
+const print = (string) => console.log(string);
 
-makePromise('file1.txt', "readFile", "")
+makePromise('file1.txt', "readFile")
  .then(result => { 
     print("---------------------------------------------");
     print(result.toString());
     return makePromise('file2.txt', "appendFile", result) 
   })
- .then(result => makePromise('file2.txt', "readFile", "") )
+ .then(result => makePromise('file2.txt', "readFile"))
  .then(result => { 
     print("----------------------------------------------");
     print(result.toString());
     return makePromise('file3.txt', "writeFile", result)
   })
- .then(result => makePromise('file3.txt', "readFile", "") )
+ .then(result => makePromise('file3.txt', "readFile"))
  .then(result => { 
     print("----------------------------------------------");
     print(result.toString());
