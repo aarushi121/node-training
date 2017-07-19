cf14d2163ace7d196a9aec5ab1c802497fae7b3f
Aarushi Goyal <agoyal@grepruby.com>
2017-07-19 19:12:58 +0530

    Code bug removed

--- Assignment4/Assignment4.js
+++ Assignment4/Assignment4.js
@@ -1,31 +1,31 @@
 const fs = require('fs');
 
-function print(string) {
+const print = (string) => {
   console.log(string);
 }
 
-function checkError(error) {
+const checkError = (error) => {
   if (error) {
     print(error);
     throw error;
   }
 }
 
-function readAppendWrite(readFile, appendFile, writeFile) {
-  fs.readFile(readFile, function (errorOnFile1Read, dataFromFile1) {
+const readAppendWrite = (readFile, appendFile, writeFile) => {
+  fs.readFile(readFile, (errorOnFile1Read, dataFromFile1) => {
     checkError(errorOnFile1Read); 
     print(dataFromFile1.toString());
     print("----------------------------------------------");
 
-    fs.appendFile(appendFile, dataFromFile1, function (errorOnFile2Append) {
+    fs.appendFile(appendFile, dataFromFile1, (errorOnFile2Append) => {
       checkError(errorOnFile2Append);
   
-      fs.readFile(appendFile, function (errorOnFile2Read, dataFromFile2) {
+      fs.readFile(appendFile, (errorOnFile2Read, dataFromFile2) => {
         checkError(errorOnFile2Read);
         
-        fs.writeFile(writeFile, datafromFile2, function (errorOnFile3Write) {
+        fs.writeFile(writeFile, dataFromFile2, (errorOnFile3Write) => {
           checkError(errorOnFile3Write);
-          print(data2.toString());
+          print(dataFromFile2.toString());
           print("-----------------------------------------------");
         });
       });
